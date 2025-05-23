e6c2101a43903e03aa561c35f9e732ecb6aa86cf
nickourou <nickourou@gmail.com>
2025-05-23 20:43:49 +0300

    Delete server directory

--- server/index.js
+++
@@ -1,8 +0,0 @@
-
-const express = require('express');
-const cors = require('cors');
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.get('/', (req, res) => res.send('Shape backend running'));
-app.listen(3000, () => console.log('Server running on port 3000'));
